c63e2997abe07a4cffb8c4fbf4f26ce04019f861
Huda Yousif <hjdoski@gmail.com>
2019-10-21 18:35:34 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,5 +17,11 @@ class Customer
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end 
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self 
+  end 
+  
 end
 
