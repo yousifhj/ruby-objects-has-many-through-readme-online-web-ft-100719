44ee58265179591a3e738dad3b84e0bed5073e8c
Huda Yousif <hjdoski@gmail.com>
2019-10-21 17:57:40 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,17 @@
 class Customer
   
+  attr_accessor :name. :age 
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
 end
